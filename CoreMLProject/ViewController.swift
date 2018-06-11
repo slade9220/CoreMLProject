db1c4413b943d423cc0c219175358deab3496653
slade9220 <slade9220@gmail.com>
2018-06-11 22:57:18 +0200

    Initial Commit

---
+++ CoreMLProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLProject
+//
+//  Created by Gennaro Amura on 11/06/2018.
+//  Copyright © 2018 Gennaro Amura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
